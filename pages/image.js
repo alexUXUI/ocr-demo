3f124875ae12664543dd406d46445b17483c9f95
Alex Bennett <alexandergraybennett@gmail.com>
2022-09-27 02:45:18 -0600

    adds dep

--- pages/image.js
+++ pages/image.js
@@ -1,4 +1,4 @@
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useLoadOCR, useOCR } from '../hooks/tesseract.hook';
 
 const ImageUploader = () => {
